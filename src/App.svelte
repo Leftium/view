22255873a8c1753bf00418479b3b0c5cd67f5050
Leftium <john@leftium.com>
2020-02-06 13:59:01 -0600

    Add taskpaper outline rendering.

--- src/App.svelte
+++ src/App.svelte
@@ -4,11 +4,15 @@
 <main>
     <h1>Hello {name}!</h1>
     <p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+
+    <div id=content>
+        {@html html}
+    </div>
+
 </main>
 
 <style>
     main {
-        text-align: center;
         padding: 1em;
         max-width: 240px;
         margin: 0 auto;
@@ -19,6 +23,28 @@
         text-transform: uppercase;
         font-size: 4em;
         font-weight: 100;
+    }
+    
+    #content {
+        white-space: pre-wrap;
+        display: inline-block;
+        tab-size: 40px;
+    }
+
+    :global([data-type="project"]) { font-weight: bold; }
+    :global([data-type="note"]) { font-style: italic; color: #93a1a1; }
+    :global([data-done]) { text-decoration: line-through; }
+    :global(span[tag]) { color: #2aa198; font-weight: normal }
+
+    :global(span[link]) { position: relative;}
+    :global(span[lead]) { position: absolute; left: -10px }
+
+    :global(ul),
+    :global(li),
+    :global(p)
+    {
+        list-style-type: none;
+        margin: .38em auto;
     }
 
     @media (min-width: 640px) {
--- src/script.coffee
+++ src/script.coffee
@@ -1,1 +1,39 @@
 `export let name;`
+
+html = ''
+
+TASKPAPER_TEXT = """
+ProjectA: @tag(with value)
+	- Task1 with @baretag in middle
+	@(Nameless tag) with note after it
+	- Task2 with sub-project
+		EmptyProject:
+ProjectB:
+	- Plain task3
+	Plain note
+"""
+
+renderTaskpaperOutline  = (text, itemPath='*') ->
+    renderItem = (item) ->
+        itemLI = document.createElement('li')
+        for attribute in item.attributeNames
+             itemLI.setAttribute attribute, item.getAttribute(attribute)
+
+        indentation = ''
+        for n in [0...item.depth-1]
+            indentation += '\t'
+        itemLI.setAttribute 'depth', item.depth
+        itemLI.innerHTML = indentation + item.bodyHighlightedAttributedString
+                                             .toInlineBMLString() or '&nbsp;'
+        return itemLI.outerHTML
+
+    outline = new birchoutline.Outline.createTaskPaperOutline(text)
+    results = outline.evaluateItemPath(itemPath)
+
+    html = ''
+    for result in results
+        html += renderItem result
+
+    return html
+
+html = renderTaskpaperOutline TASKPAPER_TEXT
